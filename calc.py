8826275b600220811a0b69aa79fabccc34ace4a9
Lion Kortlepel <development@kortlepel.com>
2019-06-05 01:57:42 +0200

    fix is_prime

--- calc.py
+++ calc.py
@@ -26,7 +26,7 @@ import math
 
 def is_prime (_x : str): # bool
     x = int (_x)
-    for i in range (0, int (math.sqrt (x)) + 1):
+    for i in range (2, int (math.sqrt (x)) + 1):
         if x % i == 0:
             return int (False)
     return int (True)
